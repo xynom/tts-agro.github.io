b84d7ff421ade0a9889eb7299bc477ca32f191db
xynom <56762384+xynom@users.noreply.github.com>
2021-09-23 18:33:40 +0545

    tts-agro

---
+++ app.js
@@ -0,0 +1,20 @@
+(function () {
+    let hamburger = {
+      nav: document.querySelector("#nav"),
+      navToggle: document.querySelector(".nav-toggle"),
+  
+      initialize() {
+        this.navToggle.addEventListener("click", () => {
+          this.toggle();
+        });
+      },
+  
+      toggle() {
+        this.navToggle.classList.toggle("expanded");
+        this.nav.classList.toggle("expanded");
+      }
+    };
+  
+    hamburger.initialize();
+  })();
+   
